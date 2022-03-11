debc7274c96f7a42f81a3ee067e3a9762593517d
augooh <goodhao0618@hotmail.com>
2022-03-11 15:20:48 +0800

    好难捏 我不活辣

--- app/src/main/java/com/app/weatherapp/MainActivity.kt
+++ app/src/main/java/com/app/weatherapp/MainActivity.kt
@@ -1,38 +1,66 @@
 package com.app.weatherapp
 
+import android.R
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
-import com.app.weatherapp.databinding.ActivityMainBinding
+import android.view.View
+import android.view.animation.Animation
+import android.view.animation.LinearInterpolator
+import android.view.animation.RotateAnimation
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.navigateUp
+
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var binding: ActivityMainBinding
-
+    private var img: ImageView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        setSupportActionBar(binding.toolbar)
-
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
-
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
+        setContentView(R.layout.activity_main)
+        img = findViewById<View>(R.id.img) as ImageView
+        //用xml实现
+        /*    Animation rotate = AnimationUtils.loadAnimation(this, R.drawable.rotate_anim);
+//        LinearInterpolator lin = new LinearInterpolator();
+//        rotate.setInterpolator(lin);
+        img.setAnimation(rotate);
+       img.startAnimation(rotate);*/
+        //用Java code实现
+        val rotate = RotateAnimation(
+            0f,
+            360f,
+            Animation.RELATIVE_TO_SELF,
+            0.5f,
+            Animation.RELATIVE_TO_SELF,
+            0.5f
+        )
+        val lin = LinearInterpolator()
+        rotate.interpolator = lin
+        rotate.duration = 1500 //设置动画持续时间
+        rotate.repeatCount = -1 //设置重复次数
+        rotate.fillAfter = true //动画执行完后是否停留在执行完的状态
+        rotate.startOffset = 10 //执行前的等待时间
+        img!!.animation = rotate
     }
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//
+//        binding = ActivityMainBinding.inflate(layoutInflater)
+//        setContentView(binding.root)
+//
+//        setSupportActionBar(binding.toolbar)
+//
+//        val navController = findNavController(R.id.nav_host_fragment_content_main)
+//        appBarConfiguration = AppBarConfiguration(navController.graph)
+//        setupActionBarWithNavController(navController, appBarConfiguration)
+//
+//        binding.fab.setOnClickListener { view ->
+//            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                .setAction("Action", null).show()
+//        }
+//    }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
