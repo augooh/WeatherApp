23fe983c3c69359831d02f1bbff552a4882231c4
augooh <goodhao0618@hotmail.com>
2022-05-05 18:08:12 +0800

    0505

--- app/src/main/java/com/app/weatherapp/common/callback/ErrorCallBack.kt
+++
@@ -1,8 +0,0 @@
-package com.app.weatherapp.common.callback
-
-import com.kingja.loadsir.callback.Callback
-import com.app.weatherapp.R
-
-class ErrorCallBck : Callback() {
-    override fun onCreateView(): Int = R.layout.layout_error
-}
