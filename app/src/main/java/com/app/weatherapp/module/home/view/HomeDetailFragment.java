bc7eb8b1518458953289eb0c6fb73b3917dea0e2
S9043659 <guohao4@oppo.com>
2022-06-07 10:44:28 +0800

    0607

--- app/src/main/java/com/app/weatherapp/module/home/view/HomeDetailFragment.java
+++
@@ -1,4 +0,0 @@
-package com.app.weatherapp.module.home.view;
-
-public class HomeDetailFragment {
-}
--- app/src/main/java/com/app/weatherapp/module/home/view/HomeFragment.java
+++
@@ -1,5 +0,0 @@
-package com.app.weatherapp.module.home.view;
-import com.app.weatherapp.R;
-
-public class HomeFragment {
-}
